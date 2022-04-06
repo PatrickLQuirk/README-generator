0ea605630a7f8f9805a65578e6014742169c4d6c
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-04-06 11:39:03 -0500

    changed name question to title question

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-            if (nameInput) {
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
                 return true;
             } else {
-                console.log('Please enter the name of your project!');
+                console.log('Please enter the title of your project!');
                 return false;
             }
         }
