2f6616d729914d4b69cbd613d6a2347f5c6f33a6
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-04-08 11:17:07 -0500

    write to file, example file written

---
+++ dist/README.md
@@ -0,0 +1,32 @@
+
+# Portfolio Generator
+
+## Description
+This is a command-line application that generates a portfolio for the user based on their input
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+I don't know yet
+
+## Usage
+See video
+
+## License
+I don't like this question
+
+## Contributing
+I am not looking for contributers on this project
+
+## Tests
+I guess you can just try using it
+
+## Questions
+If you would like to contact me with any questions about the application, my GitHub username is PatrickLQuirk and my email is patrickliamquirk@yahoo.com
--- index.js
+++ index.js
@@ -121,7 +121,24 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+            
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve ({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    })
+}
 
 // // TODO: Create a function to initialize app
 // function init() {}
@@ -134,5 +151,7 @@ const promptUser = () => {
 };
 
 promptUser().then(userInput => {
-    console.log(generateMarkdown(userInput));
+    return generateMarkdown(userInput);
+}).then(markdown => {
+    writeToFile("./dist/README.md", markdown);
 });
